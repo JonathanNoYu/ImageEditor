23861f6b482306612f1f02aca87e18e24766658e
Jonathan Yu <yu.jona@northeastern.edu>
2022-06-07 15:29:02 -0400

    Plz be update

--- Assignment4/src/ImageCommands/ImageOrientation/HorizontalFlip.java
+++ Assignment4/src/ImageCommands/ImageOrientation/HorizontalFlip.java
@@ -25,7 +25,7 @@ public class HorizontalFlip implements ImageOrientation {
   public Pixel[][] output() {
     for (int row = 0; row < in.length; row++) {
       for (int col = 0; col < in[0].length; col++) {
-        this.out[row][col] = this.in[in.length - row][col];
+        this.out[row][col] = this.in[in.length - 1 - row][col];
       }
     }
     return this.out;
--- Assignment4/src/ImageCommands/ImageOrientation/VerticalFlip.java
+++ Assignment4/src/ImageCommands/ImageOrientation/VerticalFlip.java
@@ -26,7 +26,7 @@ public class VerticalFlip implements ImageOrientation {
   public Pixel[][] output() {
     for (int row = 0; row < in.length; row++) {
       for (int col = 0; col < in[0].length; col++) {
-       this.out[row][col] = this.in[row][in[0].length - col];
+       this.out[row][col] = this.in[row][in[0].length - 1 - col];
       }
     }
     return this.out;
